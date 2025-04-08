f77f5b40ef5447dc1cd3bf597ea0cb40b37c6b98
MaldonadoAlejandroWindows <15583006.clot@fje.edu>
2025-04-08 17:17:51 +0200

    Commit 1 del projecte pizza

---
+++ pizza.php
@@ -0,0 +1,7 @@
+<br />
+<b>Warning</b>:  Undefined array key "ingr" in <b>/home/collados/public_html/asix2/m14/GP1/pizza/pizza.php</b> on line <b>3</b><br />
+<br />
+<b>Fatal error</b>:  Uncaught TypeError: count(): Argument #1 ($value) must be of type Countable|array, null given in /home/collados/public_html/asix2/m14/GP1/pizza/pizza.php:4
+Stack trace:
+#0 {main}
+  thrown in <b>/home/collados/public_html/asix2/m14/GP1/pizza/pizza.php</b> on line <b>4</b><br />
